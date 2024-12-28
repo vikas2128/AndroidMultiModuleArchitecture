7bda45a90284dac83e3d4a837d488dcb357133dd
vikaspatyalcts <vikas.patyal@cloudzent.com>
2024-12-28 17:12:30 +0530

    login field scroll

--- feature/auth/src/main/java/com/sample/auth/login/LoginScreen.kt
+++ feature/auth/src/main/java/com/sample/auth/login/LoginScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -79,7 +82,8 @@ fun LoginScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
-                    .align(Alignment.Center),
+                    .verticalScroll(rememberScrollState())
+                    .imePadding(),
                 verticalArrangement = Arrangement.Center
             ) {
                 Text(
@@ -122,6 +126,7 @@ fun LoginScreen(
         }
     }
 }
+
 
 @Composable
 fun CircularProgressBar(isLoading: Boolean) {
